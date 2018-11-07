af30faa043fff8ff702ad68a97c6acebbe7ae1b5
taligoro <taligoro.umich.edu>
2018-11-06 22:09:32 -0500

    carat test without carat functionality

--- test_rpn.py
+++ test_rpn.py
@@ -5,6 +5,7 @@ class TestBasics(unittest.TestCase):
     def test_add(self):
         result = rpn.calculate("1 1 +")
         self.assertEqual(2, result)
+    
     def test_sub(self):
         result = rpn.calculate('4 3 -')
         self.assertEqual(1, result)
@@ -12,3 +13,7 @@ class TestBasics(unittest.TestCase):
     def test_toomany(self):
         with self.assertRaises(ValueError):
             result = rpn.calculate('1 2 3 +')
+
+    def test_carat(self):
+        result = rpn.calculate('2 3 ^')
+        self.assertEqual(8, result)
